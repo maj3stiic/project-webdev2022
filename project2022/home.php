7306e1ac3787d1677197bc8226b5b185980aaa9e
maj3stiic <90041668+maj3stiic@users.noreply.github.com>
2022-02-14 08:52:43 +0200

    main

---
+++ project2022/home.php
@@ -0,0 +1,15 @@
+<?php
+
+include_once 'header2.php';
+
+<div class="content">
+    <div class="titles">
+        <h1>login</h1>
+        
+    </div>
+    <img class="big-logo" src="img/AMW-white.png" alt="AMW White logo">
+</div>
+
+
+include_once 'footer2.php'
+?>
